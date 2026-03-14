b1fa4117345fa7890876b49e63831089813a259f
Ayush Saxena <ayushofficial2912@gmail.com>
2026-03-14 17:33:39 +0530

    Fix: Replace deprecated module API URLs with the new centralized Render backend URL

--- frontend/src/utils/modules.js
+++ frontend/src/utils/modules.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const backendUrl = "http://localhost:3000/api";
-const backendUrl = "https://notesera-modules-backend.onrender.com/api"; // NotesEra main
+const backendUrl = "https://notesera-back-end.onrender.com/api"; // NotesEra main
 // const backendUrl = "https://notes-era-backend.onrender.com/api"; // NotesEra alt
 
 const fetchAllRepos = async () => {
